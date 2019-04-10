9d355c18ea191d0a39cc57e9e73165db135e1a8a
Morshed <morshedalamustc22@gmail.com>
2019-04-10 23:38:09 +0600

    commit radium removed

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Radium from 'radium';
-
 import './Person.css';
 
 const person = ( props ) => {
@@ -18,4 +16,4 @@ const person = ( props ) => {
     )
 };
 
-export default Radium(person);
+export default person;
